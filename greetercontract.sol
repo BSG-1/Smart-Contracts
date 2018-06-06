72652e5b1b4aee8f72caa957a57337d17b4ad3b2
Ghataorhe <b.ghataorhe@gmail.com>
2018-06-05 21:28:22 -0700

    adds modifier and creator assignments within constructor

--- greetercontract.sol
+++ greetercontract.sol
@@ -12,6 +12,6 @@ contract Greeter {
     //this is the constructor (same name as contract) --> takes a string and saves it TO the 'greeting' string variable
     function Greeter (string _greeting) public {
         creator = msg.sender; //whoever is the sender is assigned as the 'creator'
-        greeting = _greeting;
+        greeting = _greeting; //"_" is a special character that is used in function modifiers. It returns the flow of execution to the original function that is annotated
     }
 }
