6a96dc1cbba376e3bc57886d3e8037a847c1d764
Ghataorhe <b.ghataorhe@gmail.com>
2018-06-06 15:14:55 -0700

    sets while loop using integers.length

--- iterator.sol
+++ iterator.sol
@@ -9,5 +9,11 @@ contract BasicIterator {
     function BasicIterator() {
         creator = msg.sender; //sets the creator address 
         uint8 x = 0; //initializes an 8-bit unsigned integer 'x' to zero
+
+        //while x is less than 10 (the integers variable was set to a max of 10), set integers from 0-->9 over 10 iterations
+        while (x < integers.length) {
+            integers[x] = x;
+            x++;
+        }
     }
 }
