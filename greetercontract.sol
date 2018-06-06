f782a470a5236e4dd91a6a4e32729d67522bbc14
Ghataorhe <b.ghataorhe@gmail.com>
2018-06-05 21:15:35 -0700

    adds further instruction

--- greetercontract.sol
+++ greetercontract.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.4.23; //use 0.4.23 and newer
 Goal: take a string upon start and repeat it when greet() is called
 */
 
-//define the contract
+//define the contract; same name constructor automatically called when contract created
 contract Greeter {
-
+    address creator; 
 }
