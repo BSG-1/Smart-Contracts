dbade662b6825ce13987231ad12c2e0e7dd062f7
Ghataorhe <b.ghataorhe@gmail.com>
2018-06-06 14:56:16 -0700

    initial address is creator

--- iterator.sol
+++ iterator.sol
@@ -2,3 +2,7 @@ pragma solidity ^0.4.23
 
 /* this contract is going to be a basic while-loop iterator */
 
+contract BasicIterator {
+    address creator; //saves initial address as the creator
+    
+}
