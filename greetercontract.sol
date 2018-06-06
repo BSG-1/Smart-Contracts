1bafacb89b8e80f45edfe5615c1963ec22153af9
Ghataorhe <b.ghataorhe@gmail.com>
2018-06-05 21:21:59 -0700

    adds the constructor

--- greetercontract.sol
+++ greetercontract.sol
@@ -8,4 +8,9 @@ Goal: take a string upon start and repeat it when greet() is called
 contract Greeter {
     address creator; //pliable address variable, not set to anything
     string greeting; //pliable string variable, not set to anything
+
+    //this is the constructor (same name as contract) --> takes a string and saves it TO the 'greeting' string variable
+    function Greeter (string _greeting) public {
+
+    }
 }
