c9a6cf96a6efab07ffcf6847331f0b90f3919908
Ghataorhe <b.ghataorhe@gmail.com>
2018-06-06 15:10:10 -0700

    adds uint8 save and constructor

--- iterator.sol
+++ iterator.sol
@@ -4,5 +4,9 @@ pragma solidity ^0.4.23
 
 contract BasicIterator {
     address creator; //saves initial address as the creator
-    
+    uint8[10] integers; //leave storage for 10 8-bit unsigned integers in an array
+
+    function BasicIterator() {
+        creator = msg.sender; 
+    }
 }
