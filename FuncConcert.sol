b10e0db1ca512b8f324f1c8e2a67e041296e939a
Ghataorhe <b.ghataorhe@gmail.com>
2018-06-17 20:44:23 -0700

    makes mapping public

--- FuncConcert.sol
+++ FuncConcert.sol
@@ -5,5 +5,5 @@ contract FuncConcert {
     address owner;
     uint tickets;
     uint constant price = 1 ether;
-    mapping (address => uint) purchasers;
+    mapping (address => uint) public purchasers;
 }
